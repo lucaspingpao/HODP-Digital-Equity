0706d5294434ab36556c1ea3614795b0529c5f57
Lucas <lucaspingpao@gmail.com>
2020-11-04 17:59:54 +0100

    Updates November 4

--- Digital Equity Data.R
+++ Digital Equity Data.R
@@ -4,7 +4,7 @@
 library(stringr)
 
 # Make sure to reset your working directory to HODP-Digital-Equity
-# setwd("~/Desktop/HODP-Digital-Equity")
+# setwd("~/Documents/HODP-Digital-Equity")
 data <- read.csv("digital equity.csv")
 names <- colnames(data); names
 colnames(data)[2] <- "Email"
@@ -16,9 +16,12 @@ colnames(data)[7] <- "Clients.require.device"
 colnames(data)[8] <- "Sufficient.internet"
 colnames(data)[21] <- "Number.of.clients"
 
+# Change 11-04-20
+colnames(data)[9] <- "Types.of.connection"
+
 # Section 1 ---------------------------------------------------------------
 
-section1 <- data[c(1:8, 21)]
+section1 <- data[c(1:9, 21)]
 as.numeric(section1$"Number.of.clients")
 
 serviceTypes <- c(
@@ -40,6 +43,9 @@ serviceTypes[3] <- "Community, \n Workforce, etc."
 serviceTypes[4] <- "Education (K-12)"
 serviceTypes[6] <- "Human Services/ \n Mental Health"
 
+# Change 11-04-20
+serviceTypes[5] <- "Unhoused Services \n Provider"
+
 internet <- c("Yes", "No", "I'm not sure")
 internetValues <- 1:length(internet); internetValues
 for(i in 1:length(internet)) {
@@ -58,28 +64,39 @@ for(i in 1:length(sufficient)) {
   sufficientValues[i] <- nrow(subset(section1, str_detect(section1$"Sufficient.internet", sufficient[i])))
 }
 
+connection <- c("No internet", "Cell phone data only", "Hotspot", "Comcast", "Starry", "Verizon", "Public wifi")
+connectionValues <- 1:length(connection); connectionValues
+for(i in 1:length(connection)) {
+  connectionValues[i] <- nrow(subset(section1, str_detect(section1$"Types.of.connection", connection[i])))
+}
+connection[2] <- "Cell phone \n data only"
 
 ##################################################
 # Plots
-par(bg = "#F2F2F2", ps=11)
-png(file="~/Desktop/HODP-Digital-Equity/section1_Services_Barplot.png", width=1000, height=500)
+par(bg = "#F2F2F2", ps=11, mar=c(2,2,2,2))
+png(file="~/Documents/HODP-Digital-Equity/section1_Services_Barplot.png", width=1000, height=500)
 g <- barplot(height=serviceTypesValues, names.arg=serviceTypes, las=1, ylim=c(0,20), ylab="Frequency",
-        main="What type of services does your organization provide?", col="#595959", font.main=2)
+        main="What type of services does your organization provide?", col="#595959", font.main=2, cex.main=2.25, cex.lab=1.25, cex.axis=1.5, cex.names=0.95)
 dev.off()
 
-png(file="~/Desktop/HODP-Digital-Equity/section1_Internet_Barplot.png")
+png(file="~/Documents/HODP-Digital-Equity/section1_Internet_Barplot.png")
 barplot(height=internetValues, names.arg=internet, las=1, ylim=c(0, 50), ylab="Frequency",
         main="Are you providing services that require \n that your clients use the internet?",col="#595959", font.main=2)
 dev.off()
 
-png(file="~/Desktop/HODP-Digital-Equity/section1_Device_Barplot.png")
+png(file="~/Documents/HODP-Digital-Equity/section1_Device_Barplot.png")
 barplot(height=deviceValues, names.arg=device, las=1, ylim=c(0, 50), ylab="Frequency",
         main="Are you providing services that require that \n your clients to use a digital device \n (computer, tablet, smart phone)?",col="#595959", font.main=2)
 dev.off()
 
-png(file="~/Desktop/HODP-Digital-Equity/section1_Sufficient_Barplot.png")
+png(file="~/Documents/HODP-Digital-Equity/section1_Sufficient_Barplot.png")
 barplot(height=sufficientValues, names.arg=sufficient, las=1, ylim=c(0, 50), ylab="Frequency",
         main="Do your clients have sufficient internet connectivity \n to access services and obtain essentials?",col="#595959", font.main=2)
+dev.off()
+
+png(file="~/Documents/HODP-Digital-Equity/section1_ConnectionType_Barplot.png")
+g <- barplot(height=connectionValues, names.arg=connection, las=1, ylim=c(0, 40), ylab="Frequency",
+        main="For clients without sufficient internet connection, \n what type of connection is your client using?",col="#595959", font.main=2)
 dev.off()
 ##################################################
 
